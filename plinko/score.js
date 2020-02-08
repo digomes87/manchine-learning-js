e691da41ba083bad58f7871910d207e04205cfae
Diego G <diego.gomes87@gmail.com>
2020-02-08 16:33:53 -0300

    add functions for analysis

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,36 @@
+const outputs = []
+const k = 3
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel])
+
+  console.log(outputs)
 }
 
-function runAnalysis() {
-  // Write code here to analyze stuff
+function runAnalysis(){
+  const bucket = console.log('Fails intro', bucket)
 }
 
+function knn(data, point) {
+  const bucket = _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value()
+
+  console.log(bucket)
+}
+
+function distance(pointA, pontB){
+  return Math.abs(pointA - pontB)
+}
+
+function splitDataset(data, testCount){
+  
+}
